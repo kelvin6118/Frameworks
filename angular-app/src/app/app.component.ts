6006df7a1b331e6b24abacdc8bda3ab61d33c877
Yan Wong <kelvinwong26546@gmail.com>
2022-09-01 20:35:11 +0800

    finish component

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AppService } from './app.service';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-app';
+export class AppComponent implements OnInit{
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: AppService){}
+
+  ngOnInit(): void {
+      this.getEmployees();
+  }
+
+  public getEmployees():void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[])=>{
+        this.employees = response;
+      },
+      (error:HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    )
+  }
 }
