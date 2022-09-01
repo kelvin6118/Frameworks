0daca45769b4b1435a212114f6d9daf306157d2c
Yan Wong <kelvinwong26546@gmail.com>
2022-09-01 20:53:01 +0800

    add module and setup cors

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -9,7 +10,8 @@ import { AppService } from './app.service';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [AppService],
   bootstrap: [AppComponent]
